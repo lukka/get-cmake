60012b88eadd05435ad31c1987d5d596f098baf7
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2020-12-23 12:15:10 -0300

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -2405,7 +2405,7 @@ class ToolsGetter {
     }
 }
 exports.ToolsGetter = ToolsGetter;
-ToolsGetter.CMakeVersion = '3.19.1';
+ToolsGetter.CMakeVersion = '3.19.2';
 ToolsGetter.NinjaVersion = '1.10.2';
 // Predefined URL for CMake 
 ToolsGetter.linux_x64 = `https://github.com/Kitware/CMake/releases/download/v${ToolsGetter.CMakeVersion}/cmake-${ToolsGetter.CMakeVersion}-Linux-x86_64.tar.gz`;
