be3607584d200428ac625da584e254c4c2c0a3e6
luca <681992+lukka@users.noreply.github.com>
2022-12-06 21:40:47 -0800

    ARM support for Linux and Windows: https://github.com/lukka/get-cmake/issues/63 Add validation runs on buildjet-2vcpu-ubuntu-2204-arm

--- __tests__/action_succeeded.test.ts
+++ __tests__/action_succeeded.test.ts
@@ -9,8 +9,8 @@ import * as toolcache from '@actions/tool-cache';
 import * as core from '@actions/core';
 import { InputOptions } from '@actions/core';
 
-// 10 minutes
-jest.setTimeout(10 * 60 * 1000)
+// 30 minutes
+jest.setTimeout(30 * 60 * 1000)
 
 jest.spyOn(cache, 'saveCache').mockImplementation(() =>
     Promise.resolve(0)
@@ -40,11 +40,23 @@ test('testing get-cmake action success with default cmake', async () => {
 });
 
 test('testing get-cmake action success with specific cmake versions', async () => {
-    for (var version of ["latest", "latestrc", "~3.24", "3.x", "3.20.x", "^3.22", "3.19.8", "3.18.3", "3.16.1", "3.5.2", "3.3.0", "3.1.2", "3.25.0-rc4", "3.25.0-rc3"]) {
+    for (var version of ["latest", "latestrc", "~3.24", "3.x", "3.20.x", "^3.22", "3.19.8",
+        "3.25.0-rc4", "3.25.0-rc3"]) {
         process.env.RUNNER_TEMP = os.tmpdir();
         process.env["CUSTOM_CMAKE_VERSION"] = version;
         await getcmake.main();
         expect(coreSetFailed).toBeCalledTimes(0);
         expect(coreError).toBeCalledTimes(0);
+    }
+
+    // A Linux ARM build is not available before 3.19.x
+    if (process.platform === "linux" && process.arch === "x64" ) {
+        for (var version of ["3.18.3", "3.16.1", "3.5.2", "3.3.0", "3.1.2"]) {
+            process.env.RUNNER_TEMP = os.tmpdir();
+            process.env["CUSTOM_CMAKE_VERSION"] = version;
+            await getcmake.main();
+            expect(coreSetFailed).toBeCalledTimes(0);
+            expect(coreError).toBeCalledTimes(0);
+        }
     }
 });
