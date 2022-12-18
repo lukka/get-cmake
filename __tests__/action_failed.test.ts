f406847d290244f4bf517531782be1f96b368d09
luca <681992+lukka@users.noreply.github.com>
2022-12-17 19:23:01 -0800

    the test "action failure" was succeeding but not as intented to be.

--- __tests__/action_failed.test.ts
+++ __tests__/action_failed.test.ts
@@ -7,6 +7,7 @@ import * as cache from '@actions/cache';
 import * as toolcache from '@actions/tool-cache';
 import * as core from '@actions/core';
 import * as getcmake from '../src/get-cmake';
+import path = require('path');
 
 // 10 minutes
 jest.setTimeout(10 * 60 * 1000)
@@ -20,12 +21,17 @@ jest.spyOn(cache, 'restoreCache').mockImplementation(() => {
 }
 );
 
+jest.spyOn(core, 'getBooleanInput').mockImplementation((arg: string, options: core.InputOptions | undefined): boolean => {
+    return true;
+});
+
 var coreSetFailed = jest.spyOn(core, 'setFailed');
 var coreError = jest.spyOn(core, 'error');
 var toolsCacheDir = jest.spyOn(toolcache, 'cacheDir');
 
 test('testing get-cmake action failure', async () => {
-    process.env.RUNNER_TEMP = os.tmpdir();
+    process.env.RUNNER_TEMP = path.join(os.tmpdir(), `${process.pid}`);
+    process.env.RUNNER_TOOL_CACHE = path.join(os.tmpdir(), `${process.pid}-cache`);
     await getcmake.main();
     expect(coreSetFailed).toBeCalledTimes(1);
     expect(coreError).toBeCalledTimes(0);
